77f1f721249806f3fd40be8c8c37bfbc82ee92ac
bradenkirkham <braden.kirkham@gmail.com>
2024-06-02 16:27:59 -0600

    added click event listener to submit button that turns data inside the input field into a json object.

--- Server/display.js
+++ Server/display.js
@@ -1,1 +1,11 @@
 // code to take dummy data, store in json object, and send to server on button press.
+function submitData()
+{
+    let jsonobj = {"eleval":element.value}
+    
+    console.log(jsonobj.eleval)
+}
+
+let element = document.getElementById("dummy")
+let button = document.getElementById("submit")
+button.addEventListener("click", submitData)
